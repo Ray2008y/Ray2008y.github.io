609f563e612a6417bd3f6766d5ef7814c6aee024
Ray2008y <78857589+Ray2008y@users.noreply.github.com>
2026-02-01 08:31:58 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,61 @@
+/**
+ * Created by Administrator on 2017/12/2.
+ */
+var data=[
+    [
+        "ITEMimg/i1/ITEM.jpg",
+        "ITEMimg/i1/1.png",
+        "ITEMimg/i1/2.png",
+        "ITEMimg/i1/3.png",
+        "ITEMimg/i1/4.png",
+        "ITEMimg/i1/5.png",
+        "ITEMimg/i1/6.png",
+        "ITEMimg/i1/7.png"
+    ]
+];
+var whxyInfo=[
+    [
+        {    //1
+            x:0,
+            y:0,
+            w:964,
+            h:713
+        },
+        {         //2
+            x:0,
+            y:0,
+            w:964,
+            h:713
+        },
+        {   //3
+            x:0,
+            y:0,
+            w:964,
+            h:713
+        },
+        {      //4
+            x:0,
+            y:0,
+            w:964,
+            h:713
+        },
+        {       //5
+            x:0,
+            y:0,
+            w:964,
+            h:713
+        },
+        {     //6
+            x:0,
+            y:0,
+            w:964,
+            h:713
+        },
+        {     //7
+            x:0,
+            y:0,
+            w:964,
+            h:713
+        }
+    ]
+];
